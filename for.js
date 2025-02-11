00da0c610a0d918fa4ce771dcd5e15017c04c9f3
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-11 15:05:59 -0800

    add for loop to print squares from 1-20

---
+++ for.js
@@ -0,0 +1,13 @@
+// Write a for loop that iterates from 1 - 20 and logs out the square of each number.
+// Sample Output:
+
+// 1 squared is 1
+// 2 squared is 4
+// 3 squared is 9
+// 4 squared is 16
+// [...]
+// 20 squared is 400
+
+for (let i = 1; i <= 20; i++) {
+  console.log(`${i} squared is ${i * i}`);
+}
