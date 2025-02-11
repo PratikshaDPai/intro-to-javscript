e4a10e10c43ed175f0d28367920d0b39a2b101c2
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-11 14:35:23 -0800

    add if-else nested conditionals exercise

---
+++ app.js
@@ -0,0 +1,18 @@
+// First, declare and initialize a variable named color.
+// Then, write a branching statement that console.logs the following based upon the value of color:
+// If the value is green, log 'Go'
+// If the value is yellow, log 'Slow'
+// If the value is red, log 'Stop'
+// If the value is anything else, log 'Whatever'
+// Manipulate the value of color to ensure you’re able to log all of the above values.
+
+let color = "green";
+if (color === "green") {
+  console.log("Go");
+} else if (color === "yellow") {
+  console.log("Slow");
+} else if (color === "red") {
+  console.log("Stop");
+} else {
+  console.log("Whatever");
+}
