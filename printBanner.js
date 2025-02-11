ef1bb5f93a0ad2bc9769f497b970371030f490fd
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-11 15:43:43 -0800

    add first function to print a banner

---
+++ printBanner.js
@@ -0,0 +1,10 @@
+//learning about functions
+
+function printBanner(bannerText) {
+  console.log("===================");
+  console.log(bannerText);
+  console.log("===================");
+}
+
+//invoke function
+printBanner("This is a Banner");
