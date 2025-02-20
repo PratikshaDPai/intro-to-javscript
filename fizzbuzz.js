3ace37677e2ecde62e4217a3a7606f2ddd72c6fa
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-20 14:21:40 -0800

    Add console logs to fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,6 @@
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 11, 13, 33, 49, 75];
-const fizz = nums.filter((num) => num % 3 === 0);
+const nums = [1, 2, 3, 4, 5, 6, 7, 11, 13, 15, 33, 49, 75];
+
+const fizz = nums.filter((num) => num % 3 === 0); //filter all nums divisible by 3
 const buzz = nums.filter((num) => num % 5 === 0);
 const fizzbuzz = nums.filter((num) => num % 3 === 0 && num % 5 === 0);
 
@@ -15,14 +16,15 @@ const buzzcnt = nums.reduce((cnt, nums) => {
 }, 0); //accumulator initial value
 
 const fizzBuzzArray = nums.map((num) => {
+  //new array with fizz, buzz etc replacing multiples of 3,5 etc.
   if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
   if (num % 3 === 0) return "Fizz";
   if (num % 5 === 0) return "Buzz";
   return num;
 });
 
-console.log(nums);
-console.log(fizz, fizzcnt);
-console.log(buzz);
-console.log(fizzbuzz);
-console.log(fizzBuzzArray);
+console.log(`Initial array`, nums);
+console.log(`Nums divisible by 3`, fizz, `Count`, fizzcnt);
+console.log(`Nums divisible by 5`, buzz, `Count`, buzzcnt);
+console.log(`Nums divisible by 3 and 5`, fizzbuzz);
+console.log(`New mapped array`, fizzBuzzArray);
